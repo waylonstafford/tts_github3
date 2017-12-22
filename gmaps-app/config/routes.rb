47333516f68c2d5aeaf4bb3f85fa6d0d376be0d0
waylon stafford <staffordwaylon@gmail.com>
2017-12-22 10:03:41 -0600

    first commit

---
+++ gmaps-app/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  resources :destinations
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+# AIzaSyCAeFZTYq2kUPrpxXPwrr59XVh090tQbA4
+
+# AIzaSyCfccvVaJJlMckLIh7r3qNFk8eTdsJ5UtQ
